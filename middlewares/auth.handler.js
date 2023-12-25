2fe0a45639d058b05cf179ce99102ec8e474da0d
ak1ra-vant-min <zabala1juan@gmail.com>
2023-12-25 16:21:53 -0500

    Clase 12 - Control de roles

--- middlewares/auth.handler.js
+++ middlewares/auth.handler.js
@@ -15,4 +15,32 @@ function checkApiKey(req, res, next){
 
 };
 
-module.exports = {checkApiKey};
+//middleware
+function checkAdminRole(req, res, next){
+    console.log(req.user);
+    //el user que nos llega, va a venir con un payload asociado, en ese payload esta el role del mismo
+    const user = req.user;
+    if(user.role === 'admin'){
+        next(); //si el rol esta aprobado, le damos paso
+    } else {
+        next(boom.unauthorized());
+    }
+}
+
+//para no tener una autenticación por rol, haremos esta funcion
+//Usando Closures - estamos recibiendo los roles
+function checkRoles(...roles){
+    
+    //vamos a retornar un middleware
+    return (req, res, next) => {
+        
+        const user = req.user;
+        if(roles.includes(user.role)) { //como recibiremos un array de roles, validamos si el del usuario esta incluido
+            next(); //si el rol esta aprobado, le damos paso
+        } else {
+            next(boom.unauthorized());
+        }
+    }
+}
+
+module.exports = {checkApiKey, checkAdminRole, checkRoles};
--- routes/categories.router.js
+++ routes/categories.router.js
@@ -3,21 +3,28 @@ const passport = require('passport');
 
 const CategoryService = require('./../services/category.service');
 const validatorHandler = require('./../middlewares/validator.handler');
+const { checkRoles } = require('./../middlewares/auth.handler');
 const { createCategorySchema, updateCategorySchema, getCategorySchema } = require('./../schemas/category.schema');
 
 const router = express.Router();
 const service = new CategoryService();
 
-router.get('/', async (req, res, next) => {
-  try {
-    const categories = await service.find();
-    res.json(categories);
-  } catch (error) {
-    next(error);
-  }
+router.get('/', 
+  //este endpoint tranquilamente podría ser public, sin necesidad de login
+  passport.authenticate('jwt', {session: false}),
+  checkRoles('admin','seller', 'customer'),
+    async (req, res, next) => {
+      try {
+        const categories = await service.find();
+        res.json(categories);
+      } catch (error) {
+        next(error);
+      }
 });
 
 router.get('/:id',
+  passport.authenticate('jwt', {session: false}),
+  checkRoles('admin','seller', 'customer'),
   validatorHandler(getCategorySchema, 'params'),
   async (req, res, next) => {
     try {
@@ -32,6 +39,8 @@ router.get('/:id',
 
 router.post('/',
   passport.authenticate('jwt', {session: false}), //validamos si el usuario tiene un token valido
+  //middleware 2 - revisa la capa de gestion d epermisos
+  checkRoles('admin'), //closure => function que retorna otra function
   validatorHandler(createCategorySchema, 'body'),
   async (req, res, next) => {
     try {
@@ -45,6 +54,8 @@ router.post('/',
 );
 
 router.patch('/:id',
+  passport.authenticate('jwt', {session: false}),
+  checkRoles('admin', 'seller'),
   validatorHandler(getCategorySchema, 'params'),
   validatorHandler(updateCategorySchema, 'body'),
   async (req, res, next) => {
@@ -60,6 +71,8 @@ router.patch('/:id',
 );
 
 router.delete('/:id',
+  passport.authenticate('jwt', {session: false}),
+  checkRoles('admin', 'seller'),
   validatorHandler(getCategorySchema, 'params'),
   async (req, res, next) => {
     try {
